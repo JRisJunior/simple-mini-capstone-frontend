12b1674e1ea4c5c99064fa6f8d4029a7342bcde6
JRisJunior <jeffmrubin@gmail.com>
2023-01-26 12:30:03 -0600

    mess with vue and vue syntax

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      text: "Hey this is working",
+      showInfo: false,
+      image: "https://imageio.forbes.com/blogs-images/mitchwallace/files/2018/02/mightymax-1200x1045.jpg?format=jpg&width=960"
+    };
+  },
+  created: function () {},
+  methods: {
+    toggleTheThing: function () {
+      console.log("it's happening");
+      this.showInfo = !this.showInfo
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h2>{{ text }}</h2>
+    <p>This is just plain text</p>
+    <button v-on:click="toggleTheThing">Thing toggle</button>
+    {{ showInfo }}
+    <div v-if="showInfo">
+    THIS IS THE THING
+  </div>
+    <img v-bind:src="image" />
+  </div>
+</template>
+
+<style></style>
