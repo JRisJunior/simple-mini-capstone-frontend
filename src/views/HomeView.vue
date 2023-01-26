aedb4fb83c0aff36a62f3c56af3fec971d59d4ec
JRisJunior <jeffmrubin@gmail.com>
2023-01-26 14:16:09 -0600

    add new product fields

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,8 +6,8 @@ export default {
       message: "Welcome to Vue.js!",
       text: "Hey this is working",
       showInfo: false,
-      image: "https://imageio.forbes.com/blogs-images/mitchwallace/files/2018/02/mightymax-1200x1045.jpg?format=jpg&width=960",
       products: [],
+      newProductParams: {},
     };
   },
   created: function () {
@@ -21,12 +21,26 @@ export default {
         this.products = response.data;
       });
     },
+    createProduct: function () {
+      axios.post("http://localhost:3000/products/", this.newProductParams).then((response) => {
+        console.log("product create", response);
+        this.products.push(response.data);
+        this.newProductParams = {};
+      })},
     },
   };
 </script>
 
 <template>
   <div class="home">
+    <h1>New Product</h1>
+    <div>
+      <p>Name: <input type="text" v-model="newProductParams.name" /></p>
+      <p>price: <input type="text" v-model="newProductParams.price" /></p>
+      <p>description: <input type="text" v-model="newProductParams.description" /></p>
+      <p> supplier id: <input type="text" v-model="newProductParams.supplier_id" /></p>
+      <button v-on:click="createProduct()">Create Product</button>
+    </div>
     <h1>{{ message }}</h1>
     <h2>{{ text }}</h2>
     <div v-for="product in products" v-bind:key="product.id">
