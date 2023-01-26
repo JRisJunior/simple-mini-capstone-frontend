97db26064f9f182f66c0fefa87522e71143e695e
JRisJunior <jeffmrubin@gmail.com>
2023-01-26 13:01:27 -0600

    render products index from rails api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,39 @@
 <script>
+import axios from 'axios';
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
       text: "Hey this is working",
       showInfo: false,
-      image: "https://imageio.forbes.com/blogs-images/mitchwallace/files/2018/02/mightymax-1200x1045.jpg?format=jpg&width=960"
+      image: "https://imageio.forbes.com/blogs-images/mitchwallace/files/2018/02/mightymax-1200x1045.jpg?format=jpg&width=960",
+      products: [],
     };
   },
-  created: function () {},
-  methods: {
-    toggleTheThing: function () {
-      console.log("it's happening");
-      this.showInfo = !this.showInfo
-    },
+  created: function () {
+    console.log("page loading");
+    this.indexProducts();
   },
-};
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products.json").then((response) => {
+        console.log("products index", response)
+        this.products = response.data;
+      });
+    },
+    },
+  };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
     <h2>{{ text }}</h2>
-    <p>This is just plain text</p>
-    <button v-on:click="toggleTheThing">Thing toggle</button>
-    {{ showInfo }}
-    <div v-if="showInfo">
-    THIS IS THE THING
+    <div v-for="product in products" v-bind:key="product.id">
+    <h3>{{ product.name }}</h3>
+    <p>{{ product.price }}</p>
+    <hr />
   </div>
-    <img v-bind:src="image" />
   </div>
 </template>
 
